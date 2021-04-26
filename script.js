e3c0e9fbdaf31a3ded644decc5901bea7fe1b664
yavkina <yavkina.nastya88@gmail.com>
2021-04-26 21:43:12 +0300

    add changes

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
-const menuElem = document.getElementById('dd');
-const titleElem = menuElem.querySelector('.title');
-const list1 = document.getElementById('list1');
-const list2 = document.getElementById('list2');
-const list3 = document.getElementById('list3');
-const list4 = document.getElementById('list4');
-const myField = document.getElementById('myField');
+const dropdown = document.querySelectorAll('.dropdown');
 
-function open () {
-    menuElem.classList.toggle('active');
+for (let i=0; i<dropdown.length; i++) {
+    dropdown[i].onclick = function (event) {
+        this.classList.toggle ('active');
+            if (event.target.tagName === 'li') {
+                dropdown.querySelector('input').value = event.target.innerText;
+            }
+    }
 };
 
-titleElem.addEventListener ('click', open);
-
-function write (event) {
-    if (event.target === list1) {
-        myField.value = list1
-    } if (event.target === list2) {
-        myField.value = list2
-    } if (event.target === list3) {
-        myField.value = list3
-    } if (event.target === list4) {
-        myField.value = list4
-    }  
-};
-
-menuElem.addEventListener ('click', write);
-
-function hide (event) {
-    if (event.target === list1) {
-        menuElem.hidden = true
-    } if (event.target === list2) {
-        menuElem.hidden = true
-    } if (event.target === list3) {
-        menuElem.hidden = true
-    } if (event.target === list4) {
-        menuElem.hidden = true
-    }  
-}
-
-// console.log(myField.value);
